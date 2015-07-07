9f60a3d74a191cda42250ab8064a182e4a437f78
Steven G. Harms <steven@devbootcamp.com>
2015-07-07 11:20:23 -0400

    Musician: show how to double() or mock() a collaborator class

--- spec/models/musician_spec.rb
+++ spec/models/musician_spec.rb
@@ -1,11 +1,23 @@
 require 'rails_helper'
 
 describe Musician do
-  it "says 'woo-hoo' when she receives a Stradivarius" do
-    violiniste = Musician.create name: "Violetta Torino"
-    violin = Violin.create maker: "Stradivarius"
-    violiniste.give_instrument(violin)
-    expect(violiniste.speak).to eq "woo-hoo"
+  let(:violiniste) { Musician.create name: "Violetta Torino" }
+  let(:old_violin) { double("Violin", maker: "Stradivarius") }
+  let(:shitty) { double("Violin", maker: "McCatGuts") }
+
+  context "who is very elite" do
+    it "says 'woo-hoo' when she receives a Stradivarius" do
+      violiniste.give_instrument(old_violin)
+      expect(violiniste.speak).to eq "woo-hoo"
+    end
+  end
+
+  context "who is a beginner" do
+    it "does not say woo hoo when she receives a shitty violin" do
+      violiniste = Musician.create name: "Violetta Torino"
+      violiniste.give_instrument(shitty)
+      expect(violiniste.speak).to_not eq "woo-hoo"
+    end
   end
 
   it "does not say woo hoo when she receives a shitty violin" do
